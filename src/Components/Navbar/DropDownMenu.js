b07bbf0e37059020f57b1998c81cadc24138e36a
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2022-04-27 02:53:22 +0600

    add navbar

---
+++ src/Components/Navbar/DropDownMenu.js
@@ -0,0 +1,131 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function DropDownMenu({ title }) {
+    return (
+        <Menu as="div" className="relative inline-block text-left">
+            <div>
+                {
+                    title === "starbucks" ?
+                        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-none px-4 py-2 bg-white text-sm font-medium text-gray-700  ">
+                            Starbucks
+                            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                        </Menu.Button> :
+                        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-none px-4 py-2 bg-white text-sm font-medium text-gray-700  ">
+                            John Doe
+                            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                        </Menu.Button>
+                }
+
+            </div>
+
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                {
+                    title === "starbucks" ?
+                        <Menu.Items className="origin-top-right absolute left-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Link1
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Link2
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Link3
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items> :
+                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Profile
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            Settings
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <form method="POST" action="#">
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <button
+                                                type="submit"
+                                                className={classNames(
+                                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                    'block w-full text-left px-4 py-2 text-sm'
+                                                )}
+                                            >
+                                                Sign out
+                                            </button>
+                                        )}
+                                    </Menu.Item>
+                                </form>
+                            </div>
+                        </Menu.Items>
+                }
+            </Transition>
+        </Menu>
+    )
+}
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import DropDownMenu from './DropDownMenu';
+import searchIcon from '../../asstes/Icon/Vector.svg'
+import notificationIcon from "../../asstes/Icon/notification.svg"
+import personImg from "../../asstes/Icon/person.svg"
+
+const Navbar = () => {
+    return (
+        <>
+            <section className='flex justify-between bg-white'>
+                <div className='flex items-center'>
+                    <div className='ml-5'>
+                        <DropDownMenu
+                            title="starbucks"
+                        />
+                    </div>
+                    <div class="flex justify-center mt-5 ml-28">
+                        <div class="mb-3 xl:w-96">
+                            <div class="input-group relative flex flex-wrap items-stretch w-full mb-4">
+                                <img src={searchIcon} className="absolute z-10 mt-3 ml-3" alt="" />
+                                <input type="search" class="form-control relative flex-auto min-w-0 block w-full pl-9 pr-3 py-1.5 focus:outline-none  text-base font-normal text-gray-700  bg-clip-padding border border-solid bg-gray-100 rounded transition ease-in-out m-0 " placeholder="Search" aria-label="Search" aria-describedby="button-addon2" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex items-center mr-5'>
+                    <div>
+                        <img className='mx-4' src={notificationIcon} alt="" />
+                    </div>
+                    <div>
+                        <img className='mx-1' src={personImg} alt="" />
+                    </div>
+                    <div>
+                        <DropDownMenu
+                            title="John Doe"
+                        />
+                    </div>
+                </div>
+            </section>
+
+        </>
+    );
+};
+
+export default Navbar;
